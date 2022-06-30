ae1cecbe8417a0cfe5b8133d5f82005280c4f119
kaito-aai <kaito.s0508@gmail.com>
2022-06-30 23:24:51 +0900

    solve problem4

--- main.py
+++ main.py
@@ -31,4 +31,22 @@ def problem3(num: int):
         arr.append(n)
     return arr[-1]
 
-print(problem3(600851475143))
+def problem4(num: int):
+    startNum = 10 ** num - 1
+    num1 = startNum
+    num2 = startNum
+    result = 0
+    for n1 in range(num1, 10 ** (num - 1), -1):
+        for n2 in range(num2, 10 ** (num - 1), -1):
+            target = n1 * n2
+            targetString = str(target)
+            firstHalf = targetString[0:num]
+            reversedSecondHalf = targetString[num:len(targetString)][::-1]
+            if (firstHalf != reversedSecondHalf):
+                continue
+            if (result > target):
+                continue
+            result = target
+    return result
+
+print(problem4(3))
