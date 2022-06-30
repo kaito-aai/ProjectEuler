c14bbf314667169e5bfb985d4f50087cc0236dee
kaito-aai <kaito.s0508@gmail.com>
2022-06-30 14:28:35 +0900

    solve problem1

---
+++ main.py
@@ -0,0 +1,12 @@
+def problem1(num):
+    result = 0
+    for n in range(num - 1, 0, -1):
+        if (n % 3 == 0):
+            result += n
+            continue
+        if (n % 5 == 0):
+            result += n
+            continue
+    return result
+
+print(problem1(1000))
